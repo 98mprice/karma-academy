719ea50f9f419920c17d499b27bcad9d717d215e
micah price <98mprice@gmail.com>
2018-11-26 14:10:47 +0200

    xss fix

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,7 @@
               <v-btn v-if="current_panel"
                 @click="go_back"
                 dark
-                :disabled="current_panel._id['$oid'] == '5bf948978fb0031e9697551d'"
+                :disabled="previous_panel == null"
               >
                 undo
               </v-btn>
@@ -186,7 +186,7 @@
                   <h3 style="position: absolute; left: 0; bottom: 0" class="pa-2">{{current_panel.character}}</h3>
                 </div>
                 <v-card flat style="min-height: 100px; border-radius: 0px 0px 20px 20px; text-align: left;">
-                  <p class="pa-2" v-html="current_panel.text.replace('${player_username}', 'u/' + my_username)"></p>
+                  <p class="pa-2" v-html="sanitize(current_panel.text.replace('${player_username}', 'u/' + my_username))"></p>
                   <p class="pl-2 pr-2 red--text" v-if="current_panel.love"><b>+ ❤️ {{current_panel.love}}</b> with {{current_panel.character}}</p>
                   <v-layout row wrap>
                     <template v-for="panel in next_panels">
@@ -320,6 +320,8 @@ import VueGitHubButtons from 'vue-github-buttons';
 import 'vue-github-buttons/dist/vue-github-buttons.css';
 Vue.use(VueGitHubButtons);
 
+var xssFilters = require('xss-filters');
+
 Vue.use(vBlur)
 
 import colors from 'vuetify/es5/util/colors'
@@ -415,6 +417,10 @@ export default {
     }
   },
   methods: {
+    sanitize: function(str) {
+      var str2 = str.replace(/<[^\w<>]*(?:[^<>"'\s]*:)?[^\w<>]*(?:\W*s\W*c\W*r\W*i\W*p\W*t|\W*f\W*o\W*r\W*m|\W*s\W*t\W*y\W*l\W*e|\W*s\W*v\W*g|\W*m\W*a\W*r\W*q\W*u\W*e\W*e|(?:\W*l\W*i\W*n\W*k|\W*o\W*b\W*j\W*e\W*c\W*t|\W*e\W*m\W*b\W*e\W*d|\W*a\W*p\W*p\W*l\W*e\W*t|\W*p\W*a\W*r\W*a\W*m|\W*i?\W*f\W*r\W*a\W*m\W*e|\W*b\W*a\W*s\W*e|\W*b\W*o\W*d\W*y|\W*m\W*e\W*t\W*a|\W*i\W*m\W*a?\W*g\W*e?|\W*v\W*i\W*d\W*e\W*o|\W*a\W*u\W*d\W*i\W*o|\W*b\W*i\W*n\W*d\W*i\W*n\W*g\W*s|\W*s\W*e\W*t|\W*i\W*s\W*i\W*n\W*d\W*e\W*x|\W*a\W*n\W*i\W*m\W*a\W*t\W*e)[^>\w])|(?:<\w[\s\S]*[\s\0\/]|['"])(?:formaction|style|background|src|lowsrc|ping|on(?:d(?:e(?:vice(?:(?:orienta|mo)tion|proximity|found|light)|livery(?:success|error)|activate)|r(?:ag(?:e(?:n(?:ter|d)|xit)|(?:gestur|leav)e|start|drop|over)?|op)|i(?:s(?:c(?:hargingtimechange|onnect(?:ing|ed))|abled)|aling)|ata(?:setc(?:omplete|hanged)|(?:availabl|chang)e|error)|urationchange|ownloading|blclick)|Moz(?:M(?:agnifyGesture(?:Update|Start)?|ouse(?:PixelScroll|Hittest))|S(?:wipeGesture(?:Update|Start|End)?|crolledAreaChanged)|(?:(?:Press)?TapGestur|BeforeResiz)e|EdgeUI(?:C(?:omplet|ancel)|Start)ed|RotateGesture(?:Update|Start)?|A(?:udioAvailable|fterPaint))|c(?:o(?:m(?:p(?:osition(?:update|start|end)|lete)|mand(?:update)?)|n(?:t(?:rolselect|extmenu)|nect(?:ing|ed))|py)|a(?:(?:llschang|ch)ed|nplay(?:through)?|rdstatechange)|h(?:(?:arging(?:time)?ch)?ange|ecking)|(?:fstate|ell)change|u(?:echange|t)|l(?:ick|ose))|m(?:o(?:z(?:pointerlock(?:change|error)|(?:orientation|time)change|fullscreen(?:change|error)|network(?:down|up)load)|use(?:(?:lea|mo)ve|o(?:ver|ut)|enter|wheel|down|up)|ve(?:start|end)?)|essage|ark)|s(?:t(?:a(?:t(?:uschanged|echange)|lled|rt)|k(?:sessione|comma)nd|op)|e(?:ek(?:complete|ing|ed)|(?:lec(?:tstar)?)?t|n(?:ding|t))|u(?:ccess|spend|bmit)|peech(?:start|end)|ound(?:start|end)|croll|how)|b(?:e(?:for(?:e(?:(?:scriptexecu|activa)te|u(?:nload|pdate)|p(?:aste|rint)|c(?:opy|ut)|editfocus)|deactivate)|gin(?:Event)?)|oun(?:dary|ce)|l(?:ocked|ur)|roadcast|usy)|a(?:n(?:imation(?:iteration|start|end)|tennastatechange)|fter(?:(?:scriptexecu|upda)te|print)|udio(?:process|start|end)|d(?:apteradded|dtrack)|ctivate|lerting|bort)|DOM(?:Node(?:Inserted(?:IntoDocument)?|Removed(?:FromDocument)?)|(?:CharacterData|Subtree)Modified|A(?:ttrModified|ctivate)|Focus(?:Out|In)|MouseScroll)|r(?:e(?:s(?:u(?:m(?:ing|e)|lt)|ize|et)|adystatechange|pea(?:tEven)?t|movetrack|trieving|ceived)|ow(?:s(?:inserted|delete)|e(?:nter|xit))|atechange)|p(?:op(?:up(?:hid(?:den|ing)|show(?:ing|n))|state)|a(?:ge(?:hide|show)|(?:st|us)e|int)|ro(?:pertychange|gress)|lay(?:ing)?)|t(?:ouch(?:(?:lea|mo)ve|en(?:ter|d)|cancel|start)|ime(?:update|out)|ransitionend|ext)|u(?:s(?:erproximity|sdreceived)|p(?:gradeneeded|dateready)|n(?:derflow|load))|f(?:o(?:rm(?:change|input)|cus(?:out|in)?)|i(?:lterchange|nish)|ailed)|l(?:o(?:ad(?:e(?:d(?:meta)?data|nd)|start)?|secapture)|evelchange|y)|g(?:amepad(?:(?:dis)?connected|button(?:down|up)|axismove)|et)|e(?:n(?:d(?:Event|ed)?|abled|ter)|rror(?:update)?|mptied|xit)|i(?:cc(?:cardlockerror|infochange)|n(?:coming|valid|put))|o(?:(?:(?:ff|n)lin|bsolet)e|verflow(?:changed)?|pen)|SVG(?:(?:Unl|L)oad|Resize|Scroll|Abort|Error|Zoom)|h(?:e(?:adphoneschange|l[dp])|ashchange|olding)|v(?:o(?:lum|ic)e|ersion)change|w(?:a(?:it|rn)ing|heel)|key(?:press|down|up)|(?:AppComman|Loa)d|no(?:update|match)|Request|zoom))[\s\0]*=/gi, " ")
+      return str2
+    },
     openReddit: function(username) {
       window.open('http://www.reddit.com/u/' + username)
     },
@@ -484,7 +490,6 @@ export default {
       this.dialog_character = false
       let vm = this
       $.ajax(settings).done(function (response) {
-        console.log(response);
         vm.characters.push({
           name: this.character_name,
           url: this.character_url
@@ -512,7 +517,6 @@ export default {
       this.dialog_background = false
       let vm = this
       $.ajax(settings).done(function (response) {
-        console.log(response);
         vm.backgrounds.push({
           name: this.background_name,
           url: this.background_url
@@ -552,7 +556,6 @@ export default {
       let vm = this
       $.ajax(settings).done(function (response) {
         vm.next_panels.push(response)
-        console.log(response);
         vm.text = '',
         vm.option = ''
         vm.character = ''
@@ -576,7 +579,6 @@ export default {
         "processData": false,
         "data": "{\n\t\"id\": \"-1\"\n}"
       }).done(function (response) {
-        console.log(JSON.stringify(response))
         vm.current_panel = response[0]
         let current_id = response[0]._id['$oid']
         $.ajax({
@@ -592,7 +594,6 @@ export default {
           "processData": false,
           "data": JSON.stringify({ id: current_id })
         }).done(function (response) {
-          console.log('next panels', JSON.stringify(response))
           vm.next_panels = response
           vm.loading_inital = false
         });
